3d42fb85b8f3059d2fc730910ed70493345d68b5
Chris <chr.isaac@me.com>
2017-11-08 10:48:16 -0700

    Added upper bound equation

--- GRecoS.py
+++ GRecoS.py
@@ -6,15 +6,34 @@ import math
 # Genetic Recoding Space
 # Estimates the lower bound of the potential number of genetic codes.
 
-'''sample_space: takes basic biological parameters and returns the number of possible genetic codes'''
-def sample_space(n_bases, l_codon, n_aminos):
-    n_codons = int(n_bases)**int(l_codon)
-    n_choose_a = (math.factorial(int(n_codons)))/((math.factorial(int(n_aminos)))
-*(math.factorial(int(n_codons-n_aminos))))
+'''nCr: calculates the combinations for n choose r'''
+def ncr(n, r):
+    try:
+        value = math.factorial(int(n)) / (math.factorial(int(r))*math.factorial(int(n-r)))
+    except ValueError:
+        print "Invalid Parameters: n^l must be greater than a"
+        value = 0
+    return int(value)
 
-    n_tables = int(n_choose_a)*math.factorial(n_aminos)
-    print "There are %s possible codons utilizing %s bases in groups of %s" % (n_codons, n_bases, l_codon)
-    print "%s possible genetic codes exist when %s amino acids are required" % (n_tables, n_aminos)
+'''lower_space: returns the number of possible genetic codes for biological parameters'''
+def lower_space(n_bases, l_codon, n_aminos):
+    n_codons = n_bases**l_codon
+    n_choose_a = ncr(n_codons, n_aminos)
+    n_tables = n_choose_a*math.factorial(n_aminos)
+    print "Lower Bound Equations"
+    print n_tables
+
+'''upper_space: estimates (slightly overcounting) the maximum genetic codes '''
+def upper_space(n_bases, l_codon, n_aminos):
+    nl = n_bases**l_codon
+    value = 0
+    for i in range(n_aminos, nl):
+        value += ncr(nl, i) * ncr(i, n_aminos) * math.factorial(n_aminos) * n_aminos**(i-n_aminos)
+    print "Upper Bound Equation"
+    print value
+
+
+# --- USER INPUT SECTION ---
 
 try: a = int(sys.argv[1])    # Defines the amount of nucleic acids available to a sequence,
 except: a = int(4)           # All conventional DNA or RNA bases: ATCG or AUCG.
@@ -25,5 +44,16 @@ except: b = int(3)           # Codons are 3 bases long.
 try: c = int(sys.argv[3])    # Defines the number of amino acids in the system. Related to codon degeneracy.
 except: c = int(20)         # All essential amino aicds are needed.
 
-print ""
-sample_space(a, b, c)
+
+# --- Awesome Stuff ---
+
+print "------------- GRecoS -------------"
+print "Number of Nucleic Acids: " + str(a)
+print "Length of the Codon:     " + str(b)
+print "Number of Unique Codons: " + str(a**b)
+print "Number Amino Acids:      " + str(c)
+print "\n"
+print "------------- Results -------------"
+upper_space(int(a), int(b), int(c))
+lower_space(int(a), int(b), int(c))
+print "\n"
